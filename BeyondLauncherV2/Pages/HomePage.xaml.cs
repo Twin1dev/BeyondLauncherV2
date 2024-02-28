f7a86a07d5cb6d504572e535533e7cc5fa5a338e
Twin1 <zaydinortiz@gmail.com>
2024-02-28 00:02:53 -0600

    main page

--- BeyondLauncherV2/Pages/HomePage.xaml.cs
+++ BeyondLauncherV2/Pages/HomePage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using BeyondLauncherV2.Pages;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,21 +13,18 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BeyondLauncherV2
+namespace BeyondLauncherV2.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for HomePage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class HomePage
     {
-        public MainWindow()
+        public HomePage()
         {
             InitializeComponent();
+
         }
 
-        private void NavigationItem_Click(object sender, RoutedEventArgs e)
-        {
-            _NavigationFrame.Navigate(new HomePage());
-        }
     }
 }
