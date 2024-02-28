5c9d8b6324e4d4755cae1c99958292df05729d39
Twin1 <zaydinortiz@gmail.com>
2024-02-28 09:36:50 -0600

    animations and better readability

--- BeyondLauncherV2/Pages/HomePage.xaml.cs
+++ BeyondLauncherV2/Pages/HomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BeyondLauncherV2.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +10,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Windows.UI.Core.AnimationMetrics;
 
 namespace BeyondLauncherV2.Pages
 {
@@ -23,8 +26,79 @@ namespace BeyondLauncherV2.Pages
         public HomePage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ToastUtils.ShowToast("Launching Game..", "This may take a bit.");
+
+            DependencyObject parent = VisualTreeHelper.GetParent(this);
+            while (!(parent is Window) && parent != null)
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+
+            if (parent is Window parentWindow)
+            {
+                parentWindow.WindowState = WindowState.Minimized;
+            }
+
 
         }
 
+        private void Page_Initialized(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                ScaleTransform scaleTransform = new ScaleTransform();
+
+                button.RenderTransform = scaleTransform;
+
+                DoubleAnimation animation = new DoubleAnimation
+                {
+                    To = 1.01,
+                    Duration = TimeSpan.FromSeconds(0.05),
+                };
+
+                DoubleAnimation opacityAnimation = new DoubleAnimation
+                {
+                    To = 0.85,
+                    Duration = TimeSpan.FromSeconds(0.2),
+                };
+
+                button.RenderTransform.BeginAnimation(ScaleTransform.ScaleXProperty, animation);
+                button.BeginAnimation(Button.OpacityProperty, opacityAnimation);
+            }
+        }
+
+        private void button_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                ScaleTransform scaleTransform = new ScaleTransform();
+
+                button.RenderTransform = scaleTransform;
+
+                DoubleAnimation animation = new DoubleAnimation
+                {
+                    To = 1,
+                    Duration = TimeSpan.FromSeconds(0.3),
+                };
+
+                DoubleAnimation opacityAnimation = new DoubleAnimation
+                {
+                    To = 0.75,
+                    Duration = TimeSpan.FromSeconds(0.2),
+                };
+
+                button.RenderTransform.BeginAnimation(ScaleTransform.ScaleXProperty, animation);
+                button.BeginAnimation(Button.OpacityProperty, opacityAnimation);
+            }
+        }
     }
 }
---
+++ BeyondLauncherV2/Utilities/ToastUtils.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeyondLauncherV2.Utilities
+{
+    class ToastUtils
+    {
+        public static void ShowToast(string title, string message)
+        {
+            new ToastContentBuilder()
+                .AddText(title)
+                .AddText(message)
+                .Show();
+        }
+    }
+}
